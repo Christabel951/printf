24b0bd76c734e66f8dd963cadad1acaa898d08d8
lewmonk@gmail.com <lewmonk@gmail.com>
2021-07-26 19:24:44 +0300

    Print Nums

--- holberton.h
+++ holberton.h
@@ -3,4 +3,10 @@
 
 int _printf(const char *format, ...);
 
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
 #endif
