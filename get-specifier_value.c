b96d664b60bd9e513f2a89e53d0535fff19b8026
Christabel951 <crkomo@gmail.com>
2021-07-28 16:56:03 +0300

    rename file 0-print_sc%.c and spec_func.c

---
+++ get-specifier_value.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+/*
+ * _printch - print characters to display
+ * @c: char to be printed
+ *
+ * Return: number of bytes
+ */
+int _printch(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _strlen - calc length of string.
+ *
+ * @s: pointer to a char.
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		counter++;
+	}
+	return (counter);
+}
+/*
+ * _printstr - print characters in string
+ * @str: string value to be printed
+ *
+ * Return: number of bytes
+ */
+int _printstr(char *str)
+{
+	if (str)
+		return (write(1, str, _strlen(str)));
+	return (-1);
+}
